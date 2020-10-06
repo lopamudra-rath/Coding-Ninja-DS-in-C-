b57b99c50fb46fa5e96b1f3f6ff0a36f07472924
lopamudra12 <45901399+lopamudra12@users.noreply.github.com>
2020-10-06 22:04:05 +0530

    Create Palindrome.cpp

---
+++ 10. Character_Array/Palindrome.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+bool checkPalindrome(char str[]){
+  
+}
+
+
+int main(){
+
+  char str[10000];
+  cin.getline(str,10000);
+
+  if(checkPalindrome(str)){
+  	cout << "true" << endl; 	
+  }else{
+  cout << "false" << endl;
+  }
+}
