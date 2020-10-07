dbb9a49025152c281e6cba1121d0edd5e26803b8
lopamudra12 <45901399+lopamudra12@users.noreply.github.com>
2020-10-07 18:52:48 +0530

    function Added
    The palindrome function part added.

--- 10. Character_Array/Palindrome.cpp
+++ 10. Character_Array/Palindrome.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 bool checkPalindrome(char str[]){
-  
+	int len = 0, end, middle, begin;
+    //getting the length
+	while(str[len] != '\0'){
+    len++;
+    }
+    end = len -1;
+    middle = len/2;
+    
+    for(begin = 0; begin < middle; begin++){
+    if(str[begin] != str[end]){
+    return false;
+    }
+        end--;
+    }
+    if(begin == middle){
+    return true;
+    }
+  	 
+    return false;
+   
 }
+
 
 
 int main(){
