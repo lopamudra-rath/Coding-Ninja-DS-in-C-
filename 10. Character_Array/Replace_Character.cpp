4ff816812de5565f03903dc8d328a66c4cc9a3df
lopamudra12 <45901399+lopamudra12@users.noreply.github.com>
2020-10-07 19:15:04 +0530

    Character program Added

--- 10. Character_Array/Replace_Character.cpp
+++ 10. Character_Array/Replace_Character.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-#include "Solution.h"
+
+void replaceCharacter(char input[], char c1, char c2) {
+    for(int i = 0; input[i] != '\0'; i++){
+        if(input[i] == c1){
+            input[i] = c2;
+        }
+    }  
+}
 
 int main() {
     char input[1000000];
